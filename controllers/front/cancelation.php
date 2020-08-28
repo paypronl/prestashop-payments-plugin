4908d62f2b03df41e65f3fe6d71891f69c82c04d
MarinaAndenko <marina.andenko@gmail.com>
2020-08-28 13:21:28 +0200

    Address review comment

--- controllers/front/cancelation.php
+++ controllers/front/cancelation.php
@@ -3,6 +3,6 @@
 class PayProCancelationModuleFrontController extends ModuleFrontController
 {
 	public function postProcess() {
-		Tools::redirect('index.php?controller=cart&id_cart='.$this->context->cart->id);
+		Tools::redirect('index.php?controller=cart&id_cart=' . $this->context->cart->id);
 	}
 }
